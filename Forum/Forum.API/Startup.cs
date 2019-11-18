56f1caf55c41fb30a4634d6b2de97bc88cbcb44e
Fábio Pereira <fabiopereira.fps@gmail.com>
2019-11-18 01:40:40 -0300

    Change webproject name

--- Forum/Forum.API/Startup.cs
+++ Forum/Forum.API/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Forum.Web
+namespace Forum.API
 {
     public class Startup
     {
