66d895065263b2dff86456a81b307df51bd8f3c2
Fábio Pereira <fabiopereira.fps@gmail.com>
2019-11-27 00:47:48 -0300

    Add empty application dbcontext and controllers and viewmodels folder

---
+++ Forum/Forum.Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Forum.Data.Contexts
+{
+    class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
